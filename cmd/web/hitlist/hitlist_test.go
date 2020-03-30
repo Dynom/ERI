ac89261dc37d638b117bdab2e26f1bbb30117dd2
Mark van der Velden <mark@dynom.nl>
2020-03-30 07:11:40 +0200

    Shuffling code around

--- cmd/web/hitlist/hitlist_test.go
+++ cmd/web/hitlist/hitlist_test.go
@@ -1,9 +1,6 @@
 package hitlist
 
 import (
-	"hash/adler32"
-	"math"
-	"math/rand"
 	"reflect"
 	"testing"
 	"time"
@@ -84,87 +81,6 @@ func Test_getValidDomains(t *testing.T) {
 	}
 }
 
-type FakeInt8 struct {
-	Validations int8
-}
-type FakeInt16 struct {
-	Validations int16
-}
-type FakeInt32 struct {
-	Validations int32
-}
-type FakeInt64 struct {
-	Validations int64
-}
-
-var bigMapInt8 map[string]FakeInt8
-var bigMapInt16 map[string]FakeInt16
-var bigMapInt32 map[string]FakeInt32
-var bigMapInt64 map[string]FakeInt64
-
-func BenchmarkMemoryUsage(b *testing.B) {
-
-	const mapSize = 1000
-	const keySize = 5
-	const alnum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-	var keys = make([]string, mapSize)
-	for i := 0; i < mapSize; i++ {
-		var key = make([]byte, keySize)
-		for i := uint(0); i < keySize; i++ {
-			key[i] = alnum[rand.Intn(len(alnum))]
-		}
-
-		keys[i] = string(key)
-	}
-
-	b.Run("int8", func(t *testing.B) {
-		for j := 0; j < t.N; j++ {
-			bigMapInt8 = make(map[string]FakeInt8, mapSize)
-			for _, key := range keys {
-				bigMapInt8[key] = FakeInt8{
-					Validations: math.MaxInt8,
-				}
-			}
-		}
-	})
-	b.Run("int16", func(t *testing.B) {
-		for j := 0; j < t.N; j++ {
-			bigMapInt16 = make(map[string]FakeInt16, mapSize)
-			for _, key := range keys {
-				bigMapInt16[key] = FakeInt16{
-					Validations: math.MaxInt16,
-				}
-			}
-		}
-	})
-	b.Run("int32", func(t *testing.B) {
-		for j := 0; j < t.N; j++ {
-			bigMapInt32 = make(map[string]FakeInt32, mapSize)
-			for _, key := range keys {
-				bigMapInt32[key] = FakeInt32{
-					Validations: math.MaxInt32,
-				}
-			}
-		}
-	})
-	b.Run("int64", func(t *testing.B) {
-		for j := 0; j < t.N; j++ {
-			bigMapInt64 = make(map[string]FakeInt64, mapSize)
-			for _, key := range keys {
-				bigMapInt64[key] = FakeInt64{
-					Validations: math.MaxInt64,
-				}
-			}
-		}
-	})
-
-	_ = bigMapInt8
-	_ = bigMapInt16
-	_ = bigMapInt32
-	_ = bigMapInt64
-}
-
 func TestHitList_AddEmailAddressDeadline(t *testing.T) {
 
 	validVR := validator.Result{
@@ -204,7 +120,7 @@ func TestHitList_AddEmailAddressDeadline(t *testing.T) {
 	}
 
 	ttl := time.Hour * 1
-	h := adler32.New()
+	h := mockHasher{}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -218,4 +134,29 @@ func TestHitList_AddEmailAddressDeadline(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockHasher struct {
+	v []byte
+}
+
+func (s mockHasher) Write(p []byte) (int, error) {
+	s.v = p
+	return len(p), nil
+}
+
+func (s mockHasher) Sum(_ []byte) []byte {
+	return s.v
+}
+
+func (s mockHasher) Reset() {
+
+}
+
+func (s mockHasher) Size() int {
+	return len(s.v)
+}
+
+func (s mockHasher) BlockSize() int {
+	return 128
 }
