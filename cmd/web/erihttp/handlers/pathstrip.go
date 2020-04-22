0faae8e91cc54592f8b0f277680b139ab553e615
Mark van der Velden <mark@dynom.nl>
2020-04-22 20:12:01 +0200

    Reducing the noise of the path strip middleware

--- cmd/web/erihttp/handlers/pathstrip.go
+++ cmd/web/erihttp/handlers/pathstrip.go
@@ -9,6 +9,7 @@ import (
 
 // WithPathStrip strips the path from the request URL, paths always start with a /.
 func WithPathStrip(logger logrus.FieldLogger, path string) func(h http.Handler) http.Handler {
+	logger = logger.WithField("middleware", "path_strip")
 
 	if path == "" {
 		logger.Warn("Path strip is used with empty path argument, returning an empty handler")
@@ -20,35 +21,17 @@ func WithPathStrip(logger logrus.FieldLogger, path string) func(h http.Handler) 
 	}
 
 	path = normalizeSlashes(logger, path)
-	logger = logger.WithField("middleware", "path_strip")
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger := logger.WithField(RequestID.String(), r.Context().Value(RequestID))
-			r.URL.Path = trimPath(logger, r.URL.Path, path)
+			r.URL.Path = strings.TrimPrefix(r.URL.Path, path)
 
 			h.ServeHTTP(w, r)
 		})
 	}
 }
 
-// trimPath removes the requested part from a path
-func trimPath(logger logrus.FieldLogger, requestPath, stripPath string) string {
-	if !strings.HasPrefix(requestPath, stripPath) {
-		return requestPath
-	}
-
-	trimmed := strings.TrimPrefix(requestPath, stripPath)
-	logger.WithFields(logrus.Fields{
-		"from": requestPath,
-		"to":   trimmed,
-	}).Debug("Rewrote path")
-	return trimmed
-
-}
-
 // normalizeSlashes makes sure the path starts with a `/` and doesn't end with a `/`
 func normalizeSlashes(logger logrus.FieldLogger, path string) string {
-
 	if !strings.HasPrefix(path, `/`) {
 		original := path
 		path = `/` + path
