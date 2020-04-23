b5bf6d2cefdcf194a91338f8930d198ff7493802
Mark van der Velden <mark@dynom.nl>
2020-04-23 22:01:43 +0200

    Adding a test for TestMockHasher_Sum

--- cmd/web/hitlist/test/hash_test.go
+++ cmd/web/hitlist/test/hash_test.go
@@ -27,3 +27,26 @@ func TestMockHasherReverse_Sum(t *testing.T) {
 		})
 	}
 }
+
+func TestMockHasher_Sum(t *testing.T) {
+	type args struct {
+		p []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{name: "identical output", args: args{p: []byte("foo")}, want: []byte("foo")},
+		{name: "empty", args: args{p: []byte{}}, want: []byte{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := MockHasher{}
+			if got := s.Sum(tt.args.p); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
