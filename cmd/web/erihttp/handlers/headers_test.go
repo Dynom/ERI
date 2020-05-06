ecd66b76823db7c30129f00020361f93cc62a4b9
Annelein <annelein@Anneleins-MacBook-Pro.local>
2020-05-06 20:21:43 +0200

    adding test for add headers

--- cmd/web/erihttp/handlers/headers_test.go
+++ cmd/web/erihttp/handlers/headers_test.go
@@ -27,18 +27,63 @@ func TestWithHeaders(t *testing.T) {
 }
 
 func Test_addHeaders(t *testing.T) {
+	empty := http.Header{}
+
+	d := http.Header{}
+	d.Add("Content-Type", "application/json")
+
+	s := http.Header{}
+	s.Add("Accept-Encoding", "gzip")
+
+	want := http.Header{}
+	want.Add("Content-Type", "application/json")
+	want.Add("Accept-Encoding", "gzip")
+
+	wantDup := http.Header{}
+	wantDup.Add("Content-Type", "application/json")
+	wantDup.Add("Accept-Encoding", "gzip")
+	wantDup.Add("Accept-Encoding", "gzip")
+
 	type args struct {
 		dst http.Header
 		src http.Header
 	}
 	tests := []struct {
-		name string
-		args args
+		name     string
+		args     args
+		expected http.Header
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Testing if header added",
+			args: args{
+				dst: d,
+				src: s,
+			},
+			expected: want,
+		},
+		{
+			name: "Testing with duplicate headers",
+			args: args{
+				dst: d,
+				src: s,
+			},
+			expected: wantDup,
+		},
+		{
+			name: "Testing with empty headers",
+			args: args{
+				dst: empty,
+				src: empty,
+			},
+			expected: empty,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			addHeaders(tt.args.dst, tt.args.src)
+			if !reflect.DeepEqual(tt.args.dst, tt.expected) {
+				t.Errorf("AddHeaders() dst = %v, src = %v", tt.args.dst, tt.args.src)
+			}
 		})
 	}
 }
