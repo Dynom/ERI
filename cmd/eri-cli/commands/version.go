0c4d95f68bd7c4053ceec245317ff902cd077b03
Mark van der Velden <mark@dynom.nl>
2020-06-09 12:02:56 +0200

    Adding a version command

---
+++ cmd/eri-cli/commands/version.go
@@ -0,0 +1,23 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var version string
+
+func SetVersion(v string) {
+	version = v
+}
+
+func init() {
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "The version",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println(version)
+		},
+	}
+
+	rootCmd.AddCommand(versionCmd)
+}
--- cmd/eri-cli/main.go
+++ cmd/eri-cli/main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/Dynom/ERI/cmd/eri-cli/commands"
 )
 
+var Version = "dev"
+
 func main() {
+	commands.SetVersion(Version)
 	commands.Execute()
 }
